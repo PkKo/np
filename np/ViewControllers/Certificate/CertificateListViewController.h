61b3146ffc757b56a729255d4fc0e3899671a1c8
PkKo <khayato00@gmail.com>
2015-09-15 17:53:58 +0900

    btwssl_sign 라이브러리 빌드환경 수정

---
+++ np/ViewControllers/Certificate/CertificateListViewController.h
@@ -0,0 +1,13 @@
+//
+//  CertificateListViewController.h
+//  np
+//
+//  Created by Infobank1 on 2015. 9. 15..
+//  Copyright (c) 2015년 Infobank1. All rights reserved.
+//
+
+#import "CommonViewController.h"
+
+@interface CertificateListViewController : CommonViewController
+
+@end
---
+++ np/ViewControllers/Certificate/CertificateListViewController.m
@@ -0,0 +1,37 @@
+//
+//  CertificateListViewController.m
+//  공인인증서 목록
+//
+//  Created by Infobank1 on 2015. 9. 15..
+//  Copyright (c) 2015년 Infobank1. All rights reserved.
+//
+
+#import "CertificateListViewController.h"
+
+@interface CertificateListViewController ()
+
+@end
+
+@implementation CertificateListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
