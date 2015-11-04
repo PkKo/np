f444ba10bbb8b1423c46bbec40f5ff555ba3b341
PkKo <khayato00@gmail.com>
2015-11-04 19:33:01 +0900

    image modify/add

--- np/ViewControllers/Setting/ServiceInfoViewController.m
+++ np/ViewControllers/Setting/ServiceInfoViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ServiceInfoViewController.h"
+#import "ServiceGuideViewController.h"
 
 @interface ServiceInfoViewController ()
 
@@ -14,24 +15,16 @@
 
 @implementation ServiceInfoViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
