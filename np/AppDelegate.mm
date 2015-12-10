11d3248a7f4de6a1a426154a797fb1956d801840
PkKo <khayato00@gmail.com>
2015-12-10 17:33:45 +0900

    badge 적용 중

--- np/AppDelegate.mm
+++ np/AppDelegate.mm
@@ -21,7 +21,9 @@
 @synthesize nongminBannerImg;
 @synthesize noticeBannerImg;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    NSLog(@"########## %s ##########\nuserInfo\n%@", __FUNCTION__, launchOptions);
     // Override point for customization after application launch.
     
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
@@ -41,7 +43,7 @@
     // APN 데이터를 처리한 후 라이브러리로부터 메시지를 받을 Delegate 설정
     [IBPush setApnsHelperReceiver:(AppDelegate *)[[UIApplication sharedApplication] delegate]];
     // APNS Device 등록 및 Device Token 요청
-    [IBPush registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeSound|UIRemoteNotificationTypeAlert];
+    [IBPush registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeSound];
 /*
 #if 1
 //    [IBNgmService registerUserWithAccountId:@"8C5B196D-FD00-4814-BCA6-0C19300B58F0" verifyCode:[@"8C5B196D-FD00-4814-BCA6-0C19300B58F0" dataUsingEncoding:NSUTF8StringEncoding]];
@@ -88,6 +90,7 @@
 // 푸시 설정 성공
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
+    NSLog(@"########## %s ##########\nuserInfo\n%@", __FUNCTION__, deviceToken);
     [IBPush registerApnsDeviceToken:deviceToken];
 }
 
@@ -105,6 +108,14 @@
     
     NSLog(@"########## %s ##########\nuserInfo\n%@", __FUNCTION__, userInfo);
     
+    NSInteger badgeCount = [UIApplication sharedApplication].applicationIconBadgeNumber;
+    if(badgeCount < 0)
+    {
+        badgeCount = 0;
+    }
+    badgeCount++;
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:badgeCount];
+    
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil
                                                     message:[userInfo description]
                                                    delegate:nil
@@ -115,10 +126,22 @@
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // APN(Apple Push Notification) 데이터 처리를 위해 Library에 데이터 전달
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    [IBPush apnsHandleRemoteNotification:userInfo];
+//    [IBPush apnsHandleRemoteNotification:userInfo];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+    NSLog(@"########## %s ##########\nuserInfo\n%@", __FUNCTION__, userInfo);
     
-    NSInteger badgeCount = [UIApplication sharedApplication].applicationIconBadgeNumber++;
+    NSInteger badgeCount = [UIApplication sharedApplication].applicationIconBadgeNumber;
+    if(badgeCount < 0)
+    {
+        badgeCount = 0;
+    }
+    badgeCount++;
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:badgeCount];
+    
+//    [IBPush apnsHandleRemoteNotification:userInfo];
 }
 
 - (void)timeoutError:(NSDictionary *)response
